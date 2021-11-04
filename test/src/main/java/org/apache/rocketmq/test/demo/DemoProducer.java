17d042675d3a214281cdfb84337e3332f113ac7e
gentryhuang <gentryhuang.xw@gmail.com>
2021-11-04 21:38:49 +0800

    longpolling

--- test/src/main/java/org/apache/rocketmq/test/demo/DemoProducer.java
+++ test/src/main/java/org/apache/rocketmq/test/demo/DemoProducer.java
@@ -32,7 +32,7 @@ public class DemoProducer {
                             i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
             );
 
-            msg.setDelayTimeLevel(3);
+          //  msg.setDelayTimeLevel(3);
 
             //Call send message to deliver message to one of brokers.
             SendResult sendResult = producer.send(msg, 50000);
